1ea393f1cd4df34feef04a6d373b32d8b1e0fbe8
Corinne Dey <cjdey@pbs.org>
2021-10-08 15:51:05 -0400

    update script

--- release.sh
+++ release.sh
@@ -4,19 +4,21 @@ set -euo pipefail
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+
 echo "### Running Asset Scanner"
 
-QA_OUTPUT=$(npm run qa  | tail -3 | head -n1)
+# see how many lines we are getting from the qa command. 7 means no errors were found.
+ERRORS=$(npm run qa | wc -l) 
 
 npm run qa
 
-ERRORS=$(echo $QA_OUTPUT | awk '{print $2}')
-
-if [ $ERRORS == "0" ]; then
+if [ $ERRORS == '7' ]; then
   printf "\n${GREEN}Asset Scanner Passed.${NC}\n"
 
   echo "### Building Game"
   webpack --env.prod  
+
+  echo "### Game Built"
 else
-  printf "\n${RED}Asset Scanner Failed. Please fix ${ERRORS} errors before building your release.${NC}\n"
+  printf "\n${RED}Asset Scanner Failed. Please fix errors above before building your release.${NC}\n"
 fi
